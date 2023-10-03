154304eddadf60499ed134fb018cd69548ad7c6a
Khushi Shukla <khushishukla5890@gmail.com>
2023-10-04 00:13:11 +0530

    posts

--- client/social_media/src/Pages/Homepage/Posts.jsx
+++ client/social_media/src/Pages/Homepage/Posts.jsx
@@ -1,23 +1,46 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import { Stack, Box, Typography, Divider , Paper} from '@mui/material'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import AddCommentIcon from '@mui/icons-material/AddComment';
 import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
+import axios from 'axios'
 
 const Posts = () => {
    const [posts , setPosts] = useState([])
+   const [profile, setProfile] = useState([])
+   const showProfile = async(e) => {
+
+    const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      
+      await axios.get('/users', {} , config).then(response => {setPosts([...response.data.user.posts])
+         setProfile(response.data.user) 
+         console.log(response.data.user.posts) })
+
+   }
+
+  console.log(posts)
+   useEffect(()=>{
+     showProfile()
+   },[])
   return (
-    <Stack flexDirection={"column"} justifyContent={"center"} alignItems={"center"} display={"flex"} marginTop={"30px"}>
+   
+    <Stack flexDirection={"column"} justifyContent={"space-between"} alignItems={"center"} gap={"30px"} display={"flex"} marginTop={"30px"}>
+    {posts && posts.map((val,idx)=>{
+      return(
          <Box display="flex" justifyContent="space-around" alignItems="left" flexDirection="column" width={"30rem"} minwidth={"300px"} 
           gap={"20px"} padding={"40px"} backgroundColor={"#8e98fb"}  borderRadius={"25px"}>
            <Stack display="flex" justifyContent={"left"} gap="10px" alignItems={"center"} flexDirection={"row"}>
-              <Box width="40px" height= "40px" backgroundColor="grey" padding={"20px"} borderRadius="100%">
-                   
+              <Box width="40px" height= "40px" backgroundColor="grey" borderRadius="100%">
+                   <Box component={"img"} src={profile?.profileImage?.url}  width={"40px"} height={"40px"}  borderRadius={"100%"}></Box>
               </Box>
                
-              <Typography variant="body1">Jay</Typography>
+              <Typography variant="body1">{profile.name}</Typography>
            </Stack>
-           <Typography variant='body1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel omnis doloremque praesentium soluta autem fugiat ipsam ducimus perspiciatis, harum sint veritatis numquam. Tempora, eligendi vitae blanditiis in quisquam iste sed.</Typography>
+           <Typography variant='body1'>{val.description}</Typography>
            <Divider/>
            <Stack display={"flex"} justifyContent={"space-between"} alignItems={"center"} flexDirection={"row"}>
              <FavoriteBorderIcon/> 
@@ -57,6 +80,7 @@ const Posts = () => {
            </Paper>
          
          </Box>
+    )})}
     </Stack>
   )
 }
